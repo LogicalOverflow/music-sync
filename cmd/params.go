6b7bb541a78ca83fd3311296ec5088c8c9db2009
Leon Vack <LVack@vi-solutions.de>
2018-01-28 12:43:26 +0100

    - updated .travis.yml - added version from tag/commit

--- cmd/params.go
+++ cmd/params.go
@@ -1,7 +1,24 @@
 package cmd
 
-const (
-	Version = "0.0.1"
+var (
+	Commit  = "" // set by build flag
+	Tag     = "" // set by build flag
+	Version = version()
 	Author  = "Leon Vack"
 	Email   = ""
 )
+
+func version() string {
+	v := Tag
+	if Tag == "" {
+		v = "untagged"
+	}
+	if Commit != "" {
+		c := Commit
+		if 8 < len(c) {
+			c = c[:8]
+		}
+		v += "-" + c
+	}
+	return v
+}
