0cce9436168e26102f8aee89f8af0c62ea0e777b
Leon Vack <LVack@vi-solutions.de>
2018-02-07 15:20:43 +0100

    - testing pt.14.2

--- playback/streamer_test.go
+++ playback/streamer_test.go
@@ -14,14 +14,23 @@ func TestQueuedChunk_copySample(t *testing.T) {
 		assert.Equal(t, 1024, qs.sampleN, "the %d-th chunk is not initialized sampleN 1024", i+1)
 		for j := 0; j < 7; j++ {
 			result := make([][2]float64, 128)
-			qs.copySamples(result)
-			assert.Equal(t, 128*(j+1), qs.pos, "%d-th copySample set the position wrong for the %d-th chunk", i+1, j+1)
-			assert.Equal(t, samples[128*j:128*(j+1)], result, "%d-th copySample returned the wrong samples for the %d-th chunk", i+1, j+1)
+			n := qs.copySamples(result)
+			assert.Equal(t, 128, n, "%d-th copySample returned the wrong number of copied samples for chunk %d", j+1, i+1)
+			assert.Equal(t, 128*(j+1), qs.pos, "%d-th copySample set the position wrong for the %d-th chunk", j+1, i+1)
+			assert.Equal(t, samples[128*j:128*(j+1)], result, "%d-th copySample returned the wrong samples for the %d-th chunk", j+1, i+1)
+			assert.False(t, qs.drained(), "after %d-th copySample, drained() returned true for the %d-th chunk", j+1, i+1)
 		}
 
 		result := make([][2]float64, 256)
-		qs.copySamples(result)
-		assert.Equal(t, 1024, qs.pos, "last copySample (under-filling result) set the position wrong for the %d-th chunk", i+1)
-		assert.Equal(t, samples[896:], result[:128], "last copySample (under-filling result) returned the wrong samples for the %d-th chunk", i+1)
+		n := qs.copySamples(result)
+		assert.Equal(t, 128, n, "copySample (under-filling result) returned the wrong number of copied samples for chunk %d", i+i)
+		assert.Equal(t, 1024, qs.pos, "copySample (under-filling result) set the position wrong for the %d-th chunk", i+1)
+		assert.Equal(t, samples[896:], result[:128], "copySample (under-filling result) returned the wrong samples for the %d-th chunk", i+1)
+		assert.True(t, qs.drained(), "after copySample (under-filling result), drained() returned false for the %d-th chunk", i+1)
+
+		n = qs.copySamples(result)
+		assert.Equal(t, 0, n, "last copySample (after drained) returned the wrong number of copied samples for chunk %d", i+i)
+		assert.Equal(t, 1024, qs.pos, "last copySample (after drained) set the position wrong for the %d-th chunk", i+1)
+		assert.True(t, qs.drained(), "after last copySample (after drained), drained() returned false for the %d-th chunk", i+1)
 	}
 }
